2f044b667e94310f8e99742435fce35591f41107
cdsalty <soltiscd@gmail.com>
2020-01-03 16:21:02 -0500

    added single route parameters to the routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import Food from "./Food";
 
 import "./App.css";
@@ -7,12 +8,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Food name="Kale" />
-        <Food name="Banana" />
-        <Food name="Grapes" />
+        <Route
+          exact
+          path="/food/:name"
+          // render={routeProps => <Food name="egg" stuff={routeProps} />}
+          // render={routeProps => <Food name={routeProps.match.params.name} />} // ONE WAY
+          render={routeProps => <Food {...routeProps} />} // OPTION 2: Bring in 3 seperate props ...routeProps
+        />
+        {/* OR use component and skip the extra work but it won't work if you need another prop such as 'authenicated' */}
+        {/* <Route exact path="/food/:name" component={Food} /> */}
       </div>
     );
   }
 }
 
 export default App;
+/*
+Below, will work but you I would have to hard-code every possible food that could be entered:
+<Route path="/food/kale" render={() => <Food name="kale" />} />
+<Route path="/food/chicken" render={() => <Food name="chicken" />} />
+
+Instead, we use the ' : ' to make a URL PARAMETER, in this case it's 'name'
+
+ROUTE PROPS: assigned to all render methods
+- match
+- location
+- history
+
+render={routeProps => <Food name="egg" stuff={routeProps} />}
+  stuff returns match, location, and history
+  routeProps.match.params.name will return the name in the url
+
+*/
--- src/Food.js
+++ src/Food.js
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 import "./Food.css";
 class Food extends Component {
   render() {
-    const url = `https://source.unsplash.com/1600x900/?${this.props.name}`;
+    // with option 2, added in to make necessary changes
+    const name = this.props.match.params.name; // option 2
+    // const url = `https://source.unsplash.com/1600x900/?${this.props.name}`;
+    const url = `https://source.unsplash.com/1600x900/?${name}`; // option 2
     return (
       <div className="Food">
         <h3>I love to eat {this.props.name}</h3>
