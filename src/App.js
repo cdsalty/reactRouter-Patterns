bd4bc7bdfcab8fc64c4b97cff0d27198456e40b7
cdsalty <soltiscd@gmail.com>
2020-01-03 23:53:47 -0500

    added 404 route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Food from "./Food";
 import Meal from "./Meal";
 import "./App.css";
@@ -8,13 +8,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route
-          exact
-          path="/food/:name"
-          // render={routeProps => <Food name={routeProps.match.params.name} />} // ONE WAY
-          render={routeProps => <Food {...routeProps} />} // OPTION 2: Bring in 3 seperate props ...routeProps
-        />
-        <Route exact path="/food/:foodName/drink/:drinkName" component={Meal} />
+        {/* add switch so we only render one route, the first one to match */}
+        {/* *******   ORDER IS IMPORTANT   ********* */}
+        <Switch>
+          <Route
+            exact
+            path="/food/:name"
+            // render={routeProps => <Food name={routeProps.match.params.name} />} // ONE WAY
+            render={routeProps => <Food {...routeProps} />} // OPTION 2: Bring in 3 seperate props ...routeProps
+          />
+          <Route
+            exact
+            path="/food/:foodName/drink/:drinkName"
+            component={Meal}
+          />
+          <Route exact path="/" render={() => <h1>HomePage</h1>} />
+          <Route render={() => <h2>404 Error: Site requested not found</h2>} />
+        </Switch>
       </div>
     );
   }
